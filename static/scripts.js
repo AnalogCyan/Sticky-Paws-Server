7e0d6a364c962c05b3e49b22ba8c9ff74a524d3d
AnalogCyan <git@thayn.me>
2023-07-31 19:36:51 -0500

    Refactor `loadCharacters()`

--- static/scripts.js
+++ static/scripts.js
@@ -47,36 +47,39 @@ async function loadLevels() {
  * @returns {Promise<void>} A promise that resolves when the characters are loaded and displayed.
  * @throws {Error} If the fetch request fails or an error occurs during the process.
  */
-async function loadCharacters() {
-  try {
-    const response = await fetch("/characters");
-    console.log("Fetch response:", response);
+function loadCharacters() {
+  fetch("/characters")
+    .then((response) => {
+      console.log("Fetch response:", response);
 
-    if (!response.ok) {
-      throw new Error(`Failed to fetch characters: ${response.statusText}`);
-    }
+      if (!response.ok) {
+        throw new Error(`Failed to fetch characters: ${response.statusText}`);
+      }
 
-    const characters = await response.json();
-    console.log("Fetched characters:", characters);
-    const characterList = document.getElementById("character-list");
+      return response.json();
+    })
+    .then((characters) => {
+      console.log("Fetched characters:", characters);
+      const characterList = document.getElementById("character-list");
 
-    // Clear the existing list items
-    while (characterList.firstChild) {
-      characterList.removeChild(characterList.firstChild);
-    }
+      // Clear the existing list items
+      while (characterList.firstChild) {
+        characterList.removeChild(characterList.firstChild);
+      }
 
-    // Add the fetched characters to the list
-    for (const character of characters) {
-      const listItem = document.createElement("li");
-      listItem.textContent = `${character.name.replace(
-        "characters/",
-        ""
-      )} - Uploaded: ${character.time_created}`;
-      characterList.appendChild(listItem);
-    }
-  } catch (error) {
-    console.error("Error fetching characters:", error);
-  }
+      // Add the fetched characters to the list
+      for (const character of characters) {
+        const listItem = document.createElement("li");
+        listItem.textContent = `${character.name.replace(
+          "characters/",
+          ""
+        )} - Uploaded: ${character.time_created}`;
+        characterList.appendChild(listItem);
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching characters:", error);
+    });
 }
 
 // Load levels and characters initially
